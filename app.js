cb02f9f8e88d8838e20844fcad668a97b7209708
Dan Silver <das2c3@mail.missouri.edu>
2013-05-03 18:22:23 -0500

    Added bootstrap and stock thumbnails

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
-var app = require('express')()
+var express = require('express')
+  , path = require('path')
+  , app = express()
   , server = require('http').createServer(app)
   , io = require('socket.io').listen(server);
 
 server.listen(3000);
 app.set('view engine', 'hbs');
+app.set('views', __dirname + '/views');
 app.get('/', function (req, res) {
-  res.render('client.hbs');
+  res.render('client.hbs', {stocks: stocks});
 });
+app.use(express.static(path.join(__dirname, 'public')))
 
 /*
     Keep a global list of currently connected clients
@@ -15,12 +19,22 @@ app.get('/', function (req, res) {
 var clients = [];
 
 var stocks = [
-	{ name: 'AT&T',
-	  price: 35 },
-	{ name: 'APPLE',
+	{ name: 'AMAZON',
 	  price : 450 },
+	{ name: 'APPLE',
+	  price : 750 },
+	{ name: 'DELL',
+	  price: 200 },
+	{ name: 'FACEBOOK',
+	  price: 90 },
 	{ name: 'GOOGLE',
-	  price : 850 }
+	  price : 850 },
+	{ name: 'HP',
+	  price : 10 },
+	{ name: 'MICROSOFT',
+	  price: 350 },
+	{ name: 'YAHOO',
+	  price: 90 }
 ];
 
 lorem_ipsum = "Lorem ipsum dolor sit amet consectetur adipiscing elit Maecenas egestas libero eu mauris blandit vitae imperdiet dolor lacinia Sed ac diam vel urna viverra ullamcorper Nulla enim tortor mollis at venenatis at porta dui ligula sollicitudin non mollis non tincidunt arcu Mauris volat sagittis lorem nec tincidunt Vestibulum ipsum massa Mauris sit amet purus sit amet est feugiat gravida vel sed Proin quam sem Sed laoreet massa mauris laoreet non rhoncus lit iaculis Duis non risus augue vitae sagittis metus Fusce sagittis imperdiet massa sit amet mattis Proin sed scelerisque orci Aliquam grav justo non lacus rum tincidunt Donec faucibus sollicitudin felis eu porttitor dui porttitor faucibus";
@@ -33,7 +47,7 @@ function generateRandomWord() {
 }
 
 function generateUserName() {
-	return generateRandomWord() + generateRandomWord() + Math.floor(Math.random()*50) 
+	return generateRandomWord() + Math.floor(Math.random()*50) 
 }
 
 io.sockets.on('connection', function (socket) {
