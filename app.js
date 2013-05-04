660e96751d5e4ff55e47eb453f23fffb559b7217
Dan Silver <das2c3@mail.missouri.edu>
2013-05-03 19:15:10 -0500

    Converted stocks from array to object

--- app.js
+++ app.js
@@ -8,7 +8,11 @@ server.listen(3000);
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.get('/', function (req, res) {
-  res.render('client.hbs', {stocks: stocks});
+  sendStocks = []
+  Object.keys(stocks).forEach(function(stock) {
+    sendStocks.push({name: stock, price: stocks[stock].price})
+  })
+  res.render('client.hbs', {stocks: sendStocks});
 });
 app.use(express.static(path.join(__dirname, 'public')))
 
@@ -18,24 +22,15 @@ app.use(express.static(path.join(__dirname, 'public')))
 */
 var clients = [];
 
-var stocks = [
-	{ name: 'AMAZON',
-	  price : 450 },
-	{ name: 'APPLE',
-	  price : 750 },
-	{ name: 'DELL',
-	  price: 200 },
-	{ name: 'FACEBOOK',
-	  price: 90 },
-	{ name: 'GOOGLE',
-	  price : 850 },
-	{ name: 'HP',
-	  price : 10 },
-	{ name: 'MICROSOFT',
-	  price: 350 },
-	{ name: 'YAHOO',
-	  price: 90 }
-];
+var stocks = {};
+stocks.AMAZON =  {price: 450, direction:1};
+stocks.APPLE =  {price: 750, direction:1};
+stocks.DELL = {price: 200, direction:1};
+stocks.FACEBOOK = {price: 90, direction:1};
+stocks.GOOGLE =  {price: 850, direction:1};
+stocks.HP =  {price: 10, direction:1};
+stocks.MICROSOFT = {price: 350, direction:1};
+stocks.YAHOO = {price: 90, direction:1};
 
 lorem_ipsum = "Lorem ipsum dolor sit amet consectetur adipiscing elit Maecenas egestas libero eu mauris blandit vitae imperdiet dolor lacinia Sed ac diam vel urna viverra ullamcorper Nulla enim tortor mollis at venenatis at porta dui ligula sollicitudin non mollis non tincidunt arcu Mauris volat sagittis lorem nec tincidunt Vestibulum ipsum massa Mauris sit amet purus sit amet est feugiat gravida vel sed Proin quam sem Sed laoreet massa mauris laoreet non rhoncus lit iaculis Duis non risus augue vitae sagittis metus Fusce sagittis imperdiet massa sit amet mattis Proin sed scelerisque orci Aliquam grav justo non lacus rum tincidunt Donec faucibus sollicitudin felis eu porttitor dui porttitor faucibus";
 lorem_ipsum_words = lorem_ipsum.split(" ");
@@ -57,31 +52,28 @@ io.sockets.on('connection', function (socket) {
     */
     socket.on('join', function(callback) {
     	name = generateUserName()
-        // If the name isn't in use, join the user
-        if (clients.indexOf(name) < 0) {
-
-            // Store the name, we'll use it when sending messages
-            socket.name = name;
-
             // Add the name to the global list
             clients.push(name);
 
             // Callback to the user with a successful flag and the list of clients
-            console.log('user %s has joined', name)
+            console.log('User %s has connected.', name)
             callback(true, name, stocks);
-
-        // If the name is already in use, reject the request to join
-        } else {
-            callback(false);
-        }
+    });
+    socket.on('purchase', function(info, callback) {
+    	console.log(getStockPrice(info.stock))
     });
 });
 
 setInterval(function() {
-	stocks.forEach(function(stock) {
-		stock.price += Math.random()*stock.price*0.02*(Math.random() < 0.5 ? -1 : 1);
-		stock.price = Math.round(100*stock.price)/100;
+	Object.keys(stocks).forEach(function(stock) {
+		oldPrice = stocks[stock].price;
+		stocks[stock].price += Math.random()*stocks[stock].price*0.02*(Math.random() < 0.5 ? -1 : 1);
+		stocks[stock].price = Math.round(100*stocks[stock].price)/100;
+		if (stocks[stock].price > oldPrice) {
+			stocks[stock].direction = 1;
+		} else {
+			stocks[stock].direction = -1;
+		}
 	});
-  console.log(stocks)
   io.sockets.emit('stockUpdates', { stocks: stocks });	
 }, 1000)
